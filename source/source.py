6953934c8cb28592c68e229ff03646c43b26dfac
kallimachos <kallimachos@gmail.com>
2016-06-30 12:04:49 +1000

    Python3 and encoding

--- source/source.py
+++ source/source.py
@@ -1,4 +1,5 @@
-#!/bin/python
+#!/bin/python3
+# coding: utf-8
 """Example source file."""
 
 
--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,5 @@
-#!/bin/python
+#!/bin/python3
+# coding: utf-8
 """Example test file."""
 
 import source
@@ -11,3 +12,7 @@ def test_square():
     assert source.square(3) == 9
     assert source.square(4) == 16
     assert source.square(5) == 25
+
+
+if __name__ == '__main__':
+    pass
